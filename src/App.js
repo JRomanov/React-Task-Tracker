67b70352e60f8ae7f6aab39c80d83df6f205d392
James Romanov <jamesmsncom@hotmail.com>
2022-04-01 16:12:55 -0400

    Made tasks a global state.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react"
+
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: "Appointment",
+            day: "Feb 5th at 2:30pm",
+            reminder: true
+        },
+        {
+            id: 2,
+            text: "Food shopping",
+            day: "Feb 6th at 12:30pm",
+            reminder: true  
+        },
+        {
+            id: 3,
+            text: "Watch sunset",
+            day: "Feb 6th at 7:30pm",
+            reminder: false
+        }
+    ])
   return (
     <div className="container">
       <Header/>
-      <Tasks/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
